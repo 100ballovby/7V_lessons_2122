adae24b5357f1fc1036f0360df01c07d91464114
demidraksin <greatraksin@icloud.com>
2021-11-19 13:28:58 +0300

    Adding conditions

---
+++ 10_lesson/02_условия.js
@@ -0,0 +1,11 @@
+let birthday = 2012;
+if (birthday === 2015) {
+    console.log('Вы подходите!');
+} else if (birthday > 2015) {  // иначе если
+    console.log('Так не бывает!')
+} else { // иначе
+    console.log('Вы слишком старый!')
+}
+
+
+
---
+++ 10_lesson/03_тернарный_оператор_вопрос.js
@@ -0,0 +1,8 @@
+let age = 18;
+// тернарный оператор должен сохраняться в переменной
+
+let result = age >= 18 ? 'OK' : 'NE OK';
+console.log(result);
+
+
+
