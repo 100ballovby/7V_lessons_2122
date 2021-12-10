494b128658ebc2bd317ee9d57190c6b4092e31ca
demidraksin <greatraksin@icloud.com>
2021-12-10 11:45:21 +0300

    Adding conditions

--- 11_lesson_0312/task_1.js
+++ 11_lesson_0312/task_1.js
@@ -1,11 +1,11 @@
 let number = Number(prompt('Введите число: ', ''))
 
 if (number > 0) {
-    prompt('1')
+    prompt('1');
 } else if (number === 0) {
-    prompt('0')
+    prompt('0');
 } else {
-    prompt('-1')
+    prompt('-1');
 }
 
 
