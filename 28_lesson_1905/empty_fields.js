48b7ecbcb55a9a3414c5d1d04f37e7d9bfe1c5cf
demidraksin <greatraksin@icloud.com>
2022-05-19 10:24:08 +0300

    Adding form validation errors

--- 28_lesson_1905/empty_fields.js
+++ 28_lesson_1905/empty_fields.js
@@ -6,7 +6,10 @@ let fields = form.querySelectorAll('.field');
 // querySelectorAll - находит все элементы с таким селектором и помещает их в массив
 form.addEventListener('submit', function(event) {
     event.preventDefault();  // не давать форме перезагружаться после отправки
-
+    let errors = form.querySelectorAll('.error'); // находим все div с классом error
+    for (let i = 0; i <errors.length; i++){  // перебираю все div
+        errors[i].remove(); // удаляю div со страницы
+    }
     for (let i = 0; i < fields.length; i++) {  // просматриваю каждое поле
         if (fields[i].value === '') {
             let error = document.createElement('div');
